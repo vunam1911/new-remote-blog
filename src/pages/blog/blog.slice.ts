70a89df062cb3b5f7ad138452f7b8c2c4aa0be97
Vũ Nam <namvh@phx-smartschool.com>
2025-01-17 15:54:47 +0700

    13:49

---
+++ src/pages/blog/blog.slice.ts
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+interface BlogState {
+  postId: string
+}
+
+const initialState: BlogState = {
+  postId: ''
+}
+
+const blogSlice = createSlice({
+  name: 'blog',
+  initialState,
+  reducers: {}
+})
+
+export default blogSlice.reducer
